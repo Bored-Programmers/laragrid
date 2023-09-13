b7a5c6e59109e532630199feb2b1d08904b8bec2
Matěj Černý <mat.cerny3@gmail.com>
2023-09-13 17:57:13 +0200

    init

--- app/Livewire/AdminGrid.php
+++ app/Livewire/AdminGrid.php
@@ -11,34 +11,9 @@ class AdminGrid extends Component
 
     use WithPagination;
 
-    public $search = '';
-
-    public $sortField = '';
-
-    public $sortDirection = 'asc';
-
-    public $columns = [
-        ['name' => 'name', 'label' => 'Name', 'sortable' => true],
-        ['name' => 'posts.count', 'label' => 'Posts Count', 'sortable' => true],
-    ];
-
-    public function sortBy($field)
-    {
-        $this->sortDirection = $this->sortField === $field ? ($this->sortDirection === 'asc' ? 'desc' : 'asc') : 'asc';
-        $this->sortField = $field;
-    }
-
     public function render()
     {
         $users = User::query();
-
-        if ($this->search) {
-            $users = $users->where('name', 'like', '%' . $this->search . '%'); // Adjust for other fields
-        }
-
-        if ($this->sortField) {
-            $users = $users->orderBy($this->sortField, $this->sortDirection);
-        }
 
         return view('livewire.admin-grid', [
             'users' => $users->paginate(10),
