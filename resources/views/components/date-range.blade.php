b0a2e6eff53d65eb8f7b708c573aebb0a7355953
Matěj Černý <mat.cerny3@gmail.com>
2023-10-10 16:32:40 +0200

    date range - one column

--- resources/views/components/date-range.blade.php
+++ resources/views/components/date-range.blade.php
@@ -5,5 +5,23 @@
     'theme',
 ])
 
-<input type="text" wire:model.live="filter.{{ $column->getModelField() }}.from" class="{{ $theme->getFilterDate() }}"> -
-<input type="text" wire:model.live="filter.{{ $column->getModelField() }}.to" class="{{ $theme->getFilterDate() }}">
+@php($filter = $column->getFilter())
+
+@if ($filter instanceof \BoredProgrammers\LaraGrid\Filters\DateRangeFilter && $filter->isOneInput())
+    <input
+            type="text"
+            wire:model.live="filter.{{ $column->getModelField() }}"
+            class="{{ $theme->getFilterDate() }}"
+    >
+@else
+    <input
+            type="text"
+            wire:model.live="filter.{{ $column->getModelField() }}.from"
+            class="{{ $theme->getFilterDate() }}"
+    > -
+    <input
+            type="text"
+            wire:model.live="filter.{{ $column->getModelField() }}.to"
+            class="{{ $theme->getFilterDate() }}"
+    >
+@endif
--- src/Filters/DateRangeFilter.php
+++ src/Filters/DateRangeFilter.php
@@ -8,13 +8,28 @@ use BoredProgrammers\LaraGrid\Enums\FiltrationType;
 class DateRangeFilter extends BaseFilter
 {
 
+    protected bool $isOneInput = false;
+
     public static function make($isOneInput = false): self
     {
         $filter = new static();
         $filter->setFiltrationType(FiltrationType::DATE_BETWEEN);
         $filter->setFilterType(FilterType::DATE_RANGE);
+        $filter->setIsOneInput($isOneInput);
 
         return $filter;
+    }
+
+    public function isOneInput(): bool
+    {
+        return $this->isOneInput;
+    }
+
+    public function setIsOneInput(bool $isOneInput): DateRangeFilter
+    {
+        $this->isOneInput = $isOneInput;
+
+        return $this;
     }
 
 }
