5fd90fb46047e53669b0dfbbcc2268e99a23ef90
Matěj Černý <mat.cerny3@gmail.com>
2023-10-10 16:21:26 +0200

    date range

--- src/Filters/DateFilter.php
+++ src/Filters/DateFilter.php
@@ -5,10 +5,10 @@ namespace BoredProgrammers\LaraGrid\Filters;
 use BoredProgrammers\LaraGrid\Enums\FilterType;
 use BoredProgrammers\LaraGrid\Enums\FiltrationType;
 
-class DateRangeFilter extends BaseFilter
+class DateFilter extends BaseFilter
 {
 
-    public static function make($isOneInput = false): self
+    public static function make(): self
     {
         $column = new static();
         $column->setFiltrationType(FiltrationType::DATE_BETWEEN);
